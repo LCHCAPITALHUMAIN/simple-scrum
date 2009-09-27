b3671afe4972eee2070bcb1f620f1769d181114a
jeanphilippe.hallot@gmail.com <jeanphilippe.hallot@gmail.com@b9785878-aa22-11de-8882-bb5941c2e707>
2009-09-27 20:45:35 +0000

    create structures

--- simpleScrum/trunk/database/sprintBacklog_Mysql.sql
+++ simpleScrum/trunk/database/sprintBacklog_Mysql.sql
@@ -1,61 +1,89 @@
 ﻿
 CREATE DATABASE  `simpleScrum` ;
 
+-- phpMyAdmin SQL Dump
+-- version 2.11.6
+-- http://www.phpmyadmin.net
+--
+-- Serveur: localhost
+-- Généré le : Mar 14 Juillet 2009 à 21:11
+-- Version du serveur: 5.0.51
+-- Version de PHP: 5.2.6
 
-DROP TABLE sprintbacklog;
 
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
 
-CREATE TABLE sprintbacklog (
-  `Backlog Item ID` varchar(255) NULL default NULL,
-  `typeTask2` varchar(255) NULL default NULL,
-  `User Story` varchar(255) NULL default NULL,
-  `Team` varchar(255) NULL default NULL,
-  `Sprint #` varchar(255) NULL default NULL,
-  `CRDS Web Module Sub Module` varchar(255) NULL default NULL,
-  `Budget` varchar(255) NULL default NULL,
-  `Task` varchar(255) NULL default NULL,
-  `Estimate` varchar(255) NULL default NULL,
-  `Discrepency` varchar(255) NULL default NULL,
-  `Remarks` varchar(4000) NULL default NULL,
-  `Actual` varchar(255) NULL default NULL,
-  `Jira Task Number` varchar(255) NULL default NULL,
-  `Status` varchar(255) NULL default NULL,
-  `Metric` varchar(255) NULL default NULL,
-  `Day 1` varchar(255) NULL default NULL,
-  `Day 2` varchar(255) NULL default NULL,
-  `Day 3` varchar(255) NULL default NULL,
-  `Day 4` varchar(255) NULL default NULL,
-  `Day 5` varchar(255) NULL default NULL,
-  `Day 6` varchar(255) NULL default NULL,
-  `Day 7` varchar(255) NULL default NULL,
-  `Day 8` varchar(255) NULL default NULL,
-  `Day 9` varchar(255) NULL default NULL,
-  `Day 10` varchar(255) NULL default NULL,
-  `Dhanya` varchar(255) NULL default NULL,
-  `Kunal` varchar(255) NULL default NULL,
-  `Sourabh` varchar(255) NULL default NULL,
-  `Nazir` varchar(255) NULL default NULL,
-  `Imad` varchar(255) NULL default NULL,
-  `Luciano` varchar(255) NULL default NULL,
-  `Pranav` varchar(255) NULL default NULL,
-  `Tushar` varchar(255) NULL default NULL,
-  `Ashish` varchar(255) NULL default NULL,
-  `Jphi` varchar(255) NULL default NULL,
-  `Yann` varchar(255) NULL default NULL,
-  `Florent` varchar(255) NULL default NULL,
-  `Day 1b` varchar(255) NULL default NULL,
-  `Day 2b` varchar(255) NULL default NULL,
-  `Day 3b` varchar(255) NULL default NULL,
-  `Day 4b` varchar(255) NULL default NULL,
-  `Day 5b` varchar(255) NULL default NULL,
-  `Day 6b` varchar(255) NULL default NULL,
-  `Day 7b` varchar(255) NULL default NULL,
-  `Day 8b` varchar(255) NULL default NULL,
-  `Day 9b` varchar(255) NULL default NULL,
-  `Day 10b` varchar(255) NULL default NULL,
-  `Estimatesb` varchar(255) NULL default NULL,
-  `Estimates Completed` varchar(255) NULL default NULL
-) ENGINE = INNODB;
+--
+-- Base de données: `scrum`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `sprintbacklog`
+--
+
+DROP TABLE IF EXISTS `sprintbacklog`;
+CREATE TABLE IF NOT EXISTS `sprintbacklog` (
+  `TaskUniqueID` bigint(20) NOT NULL auto_increment,
+  `BacklogItemID` varchar(255) default NULL,
+  `typeTask2` varchar(255) default NULL,
+  `UserStory` varchar(255) default NULL,
+  `Team` varchar(255) default NULL,
+  `SprintNo` varchar(255) default NULL,
+  `CRDSWebModuleSubModule` varchar(255) default NULL,
+  `Budget` varchar(255) default NULL,
+  `Task` varchar(255) default NULL,
+  `Estimate` varchar(255) default NULL,
+  `Discrepency` varchar(255) default NULL,
+  `Remarks` varchar(255) default NULL,
+  `Actual` varchar(255) default NULL,
+  `JiraTaskNumber` varchar(255) default NULL,
+  `Status` varchar(255) default NULL,
+  `Metric` varchar(255) default NULL,
+  `Day1` varchar(255) default NULL,
+  `Day2` varchar(255) default NULL,
+  `Day3` varchar(255) default NULL,
+  `Day4` varchar(255) default NULL,
+  `Day5` varchar(255) default NULL,
+  `Day6` varchar(255) default NULL,
+  `Day7` varchar(255) default NULL,
+  `Day8` varchar(255) default NULL,
+  `Day9` varchar(255) default NULL,
+  `Day10` varchar(255) default NULL,
+  `Dhanya` varchar(255) default NULL,
+  `Kunal` varchar(255) default NULL,
+  `Sourabh` varchar(255) default NULL,
+  `Nazir` varchar(255) default NULL,
+  `Imad` varchar(255) default NULL,
+  `Luciano` varchar(255) default NULL,
+  `Pranav` varchar(255) default NULL,
+  `Tushar` varchar(255) default NULL,
+  `Ashish` varchar(255) default NULL,
+  `Jphi` varchar(255) default NULL,
+  `Yann` varchar(255) default NULL,
+  `Florent` varchar(255) default NULL,
+  `Day1b` varchar(255) default NULL,
+  `Day2b` varchar(255) default NULL,
+  `Day3b` varchar(255) default NULL,
+  `Day4b` varchar(255) default NULL,
+  `Day5b` varchar(255) default NULL,
+  `Day6b` varchar(255) default NULL,
+  `Day7b` varchar(255) default NULL,
+  `Day8b` varchar(255) default NULL,
+  `Day9b` varchar(255) default NULL,
+  `Day10b` varchar(255) default NULL,
+  `Estimatesb` varchar(255) default NULL,
+  `EstimatesCompleted` varchar(255) default NULL,
+  PRIMARY KEY  (`TaskUniqueID`)
+) TYPE=InnoDB  AUTO_INCREMENT=1977 ;
+
+--
+-- Contenu de la table `sprintbacklog`
+--
 
 
 INSERT INTO sprintbacklog VALUES('15', 'Planned', 'Setup new integration server', '1', 'Sprint 3', '', 'W', 'First test', '1', '1', '', '0', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', null, null, null, null, null, null, null, null, null, null, '1', '0');
---
+++ simpleScrum/trunk/database/sprintBacklog_Oracle.sql
@@ -0,0 +1,82 @@
+﻿
+set define off
+
+create user sscrum identified by sscrum default tablespace users;
+grant resource to sscrum;
+grant connect to sscrum;
+
+
+--
+-- Structure de la table `sprintbacklog`
+--
+
+DROP TABLE sprintbacklog;
+CREATE TABLE sprintbacklog (
+  "TaskUniqueID" number(20) NOT NULL,
+  "BacklogItemID" varchar(255) default NULL,
+  "typeTask2" varchar(255) default NULL,
+  "UserStory" varchar(255) default NULL,
+  "Team" varchar(255) default NULL,
+  "SprintNo" varchar(255) default NULL,
+  "CRDSWebModuleSubModule" varchar(255) default NULL,
+  "Budget" varchar(255) default NULL,
+  "Task" varchar(255) default NULL,
+  "Estimate" varchar(255) default NULL,
+  "Discrepency" varchar(255) default NULL,
+  "Remarks" varchar(255) default NULL,
+  "Actual" varchar(255) default NULL,
+  "JiraTaskNumber" varchar(255) default NULL,
+  "Status" varchar(255) default NULL,
+  "Metric" varchar(255) default NULL,
+  "Day1" varchar(255) default NULL,
+  "Day2" varchar(255) default NULL,
+  "Day3" varchar(255) default NULL,
+  "Day4" varchar(255) default NULL,
+  "Day5" varchar(255) default NULL,
+  "Day6" varchar(255) default NULL,
+  "Day7" varchar(255) default NULL,
+  "Day8" varchar(255) default NULL,
+  "Day9" varchar(255) default NULL,
+  "Day10" varchar(255) default NULL,
+  "Dhanya" varchar(255) default NULL,
+  "Kunal" varchar(255) default NULL,
+  "Sourabh" varchar(255) default NULL,
+  "Nazir" varchar(255) default NULL,
+  "Imad" varchar(255) default NULL,
+  "Luciano" varchar(255) default NULL,
+  "Pranav" varchar(255) default NULL,
+  "Tushar" varchar(255) default NULL,
+  "Ashish" varchar(255) default NULL,
+  "Jphi" varchar(255) default NULL,
+  "Yann" varchar(255) default NULL,
+  "Florent" varchar(255) default NULL,
+  "Day1b" varchar(255) default NULL,
+  "Day2b" varchar(255) default NULL,
+  "Day3b" varchar(255) default NULL,
+  "Day4b" varchar(255) default NULL,
+  "Day5b" varchar(255) default NULL,
+  "Day6b" varchar(255) default NULL,
+  "Day7b" varchar(255) default NULL,
+  "Day8b" varchar(255) default NULL,
+  "Day9b" varchar(255) default NULL,
+  "Day10b" varchar(255) default NULL,
+  "Estimatesb" varchar(255) default NULL,
+  "EstimatesCompleted" varchar(255) default NULL,
+  PRIMARY KEY  ("TaskUniqueID")
+) ;
+
+drop sequence sq_sprintbacklog;
+create sequence sq_sprintbacklog start with 5;
+
+--
+-- Contenu de la table `sprintbacklog`
+--
+
+INSERT INTO sprintbacklog VALUES(1, '15', 'Planned', 'Setup new integration server', '1', 'Sprint 3', '', 'W', 'First test', '1', '1', '', '0', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', null, null, null, null, null, null, null, null, null, null, '1', '0');
+INSERT INTO sprintbacklog VALUES(2, '15', 'Planned', 'Setup new integration server', '1', 'Sprint 3', '', 'W', 'Define our standards', '1', '1', '', '0', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', null, null, null, null, null, null, null, null, null, null, '1', '0');
+INSERT INTO sprintbacklog VALUES(3, '15', 'Planned', 'Setup new integration server', '1', 'Sprint 3', '', 'W', 'Train everybody using this new integration server', '1', '1', '', '0', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', null, null, null, null, null, null, null, null, null, null, '1', '0');
+INSERT INTO sprintbacklog VALUES(4, '15', 'Planned', 'Setup new integration server', '1', 'Sprint 3', '', 'W', 'Train everybody managing this new integration server', '1', '1', '', '0', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', null, null, null, null, null, null, null, null, null, null, '1', '0');
+
+commit;
+
+set define on
