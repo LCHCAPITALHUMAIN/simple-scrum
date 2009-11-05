f0fcdd1275b88c4f65bd0adf28ab02a78592feaf
Imad.ELALI <Imad.ELALI@b9785878-aa22-11de-8882-bb5941c2e707>
2009-11-05 01:41:31 +0000

    -Adding audit trail support. Code from the plugin "audit-trail' with some minor changes (mainly adding a register of hibernate events on BootStrap)

--- trunk/SimpleScrum/grails-app/conf/BootStrap.groovy
+++ trunk/SimpleScrum/grails-app/conf/BootStrap.groovy
@@ -1,7 +1,50 @@
+import org.springframework.web.context.support.WebApplicationContextUtils
+import com.simplescrum.AuditLogListener
+import org.codehaus.groovy.grails.commons.ApplicationHolder
+import org.hibernate.event.EventListeners
+
 class BootStrap {
 
-     def init = { servletContext ->
-     }
-     def destroy = {
-     }
+  def init = {servletContext ->
+    def webAppCtx = WebApplicationContextUtils.getWebApplicationContext(servletContext)
+    doWithApplicationContext(webAppCtx)
+  }
+  def destroy = {
+  }
+
+  def doWithApplicationContext = {webAppCtx ->
+
+    def sessionFactory  = webAppCtx.getBean('sessionFactory')
+    EventListeners eventListeners = sessionFactory.eventListeners
+
+    def listener = new AuditLogListener()
+    def config = ApplicationHolder.application.config.auditLog
+    if (config?.verbose) {
+      listener.verbose = (config?.verbose) ? true : false
+    }
+    if (config?.actor) {
+      listener.actorKey = (config?.actor) ?: config?.user
+    }
+    if (config?.username) {
+      listener.sessionAttribute = (config?.username) ?: null
+    }
+
+    // use preDelete so we can see what is going to be destroyed
+    // hook to the postInsert to grab the ID of the object
+    // hook to postUpdate to use the old and new state hooks
+    ['preDelete', 'postInsert', 'postUpdate',].each({
+      addEventTypeListener(eventListeners, listener, it)
+    })
+  }
+
+  // Brazenly lifted from HibernateEventsGrailsPlugin by Kevin Burke
+  def addEventTypeListener(listeners, listener, type) {
+    def typeProperty = "${type}EventListeners"
+    def typeListeners = listeners."${typeProperty}"
+    def expandedTypeListeners = new Object[typeListeners.length + 1]
+    System.arraycopy(typeListeners, 0, expandedTypeListeners, 0, typeListeners.length)
+    expandedTypeListeners[-1] = listener
+    listeners."${typeProperty}" = expandedTypeListeners
+  }
+
 } 
