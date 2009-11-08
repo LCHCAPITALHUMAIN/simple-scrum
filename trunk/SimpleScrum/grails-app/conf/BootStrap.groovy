59cf4f44ee156635a0027b9b4d451565b81ba747
Imad.ELALI <Imad.ELALI@b9785878-aa22-11de-8882-bb5941c2e707>
2009-11-08 14:35:27 +0000

    -BootStrap: ->Added the possibility to launch the grails console alongside the application. This will allow playing with the data and seeing the results directly live in the application. ->Added new environment 'custom' with a 'create-drop' sperarte DB 'consoleDB' and with the flag 'runConsole' to allox the console launch when true.

--- trunk/SimpleScrum/grails-app/conf/BootStrap.groovy
+++ trunk/SimpleScrum/grails-app/conf/BootStrap.groovy
@@ -1,5 +1,7 @@
 import org.springframework.web.context.support.WebApplicationContextUtils
 import com.simplescrum.AuditLogListener
+import org.codehaus.groovy.grails.commons.GrailsApplication;
+import groovy.ui.Console
 import org.codehaus.groovy.grails.commons.ApplicationHolder
 import org.hibernate.event.EventListeners
 
@@ -8,13 +10,27 @@ class BootStrap {
   def init = {servletContext ->
     def webAppCtx = WebApplicationContextUtils.getWebApplicationContext(servletContext)
     doWithApplicationContext(webAppCtx)
+    runConsoleIfNeeded(webAppCtx)
   }
   def destroy = {
   }
 
+  def runConsoleIfNeeded = {webAppCtx ->
+    def config = ApplicationHolder.application.config
+
+    if (config?.runConsole) {
+      // launch a groovy console
+      def grailsApp = webAppCtx.getBean(GrailsApplication.APPLICATION_ID);
+      Binding b = new Binding();
+      b.setVariable("ctx", webAppCtx);
+      def console = new Console(grailsApp.classLoader, b);
+      console.run()
+    }
+  }
+
   def doWithApplicationContext = {webAppCtx ->
 
-    def sessionFactory  = webAppCtx.getBean('sessionFactory')
+    def sessionFactory = webAppCtx.getBean('sessionFactory')
     EventListeners eventListeners = sessionFactory.eventListeners
 
     def listener = new AuditLogListener()
