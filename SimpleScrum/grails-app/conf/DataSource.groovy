19e4a53008110bb6929596ae13ce397b312cae84
Imad.ELALI <Imad.ELALI@b9785878-aa22-11de-8882-bb5941c2e707>
2009-11-08 14:35:27 +0000

    -BootStrap: ->Added the possibility to launch the grails console alongside the application. This will allow playing with the data and seeing the results directly live in the application. ->Added new environment 'custom' with a 'create-drop' sperarte DB 'consoleDB' and with the flag 'runConsole' to allox the console launch when true.

--- SimpleScrum/grails-app/conf/DataSource.groovy
+++ SimpleScrum/grails-app/conf/DataSource.groovy
@@ -1,32 +1,39 @@
 dataSource {
-	pooled = true
-	driverClassName = "org.hsqldb.jdbcDriver"
-	username = "sa"
-	password = ""
+  pooled = true
+  driverClassName = "org.hsqldb.jdbcDriver"
+  username = "sa"
+  password = ""
 }
 hibernate {
-    cache.use_second_level_cache=true
-    cache.use_query_cache=true
-    cache.provider_class='com.opensymphony.oscache.hibernate.OSCacheProvider'
+  cache.use_second_level_cache = true
+  cache.use_query_cache = true
+  cache.provider_class = 'com.opensymphony.oscache.hibernate.OSCacheProvider'
 }
 // environment specific settings
 environments {
-	development {
-		dataSource {
-			dbCreate = "update" // one of 'create', 'create-drop','update'
-			url = "jdbc:hsqldb:file:devDB"
-		}
-	}
-	test {
-		dataSource {
-			dbCreate = "update"
-			url = "jdbc:hsqldb:mem:testDb"
-		}
-	}
-	production {
-		dataSource {
-			dbCreate = "update"
-			url = "jdbc:hsqldb:file:prodDb;shutdown=true"
-		}
-	}
+  development {
+    dataSource {
+      dbCreate = "update" // one of 'create', 'create-drop','update'
+      url = "jdbc:hsqldb:file:devDB"
+    }
+  }
+  custom {
+    dataSource {
+      dbCreate = "create-drop" // one of 'create', 'create-drop','update'
+      url = "jdbc:hsqldb:file:consoleDB"
+    }
+    runConsole = true //When true launches Grails console alongside the application. U'll be able to query and modify data live.
+  }
+  test {
+    dataSource {
+      dbCreate = "update"
+      url = "jdbc:hsqldb:mem:testDb"
+    }
+  }
+  production {
+    dataSource {
+      dbCreate = "update"
+      url = "jdbc:hsqldb:file:prodDb;shutdown=true"
+    }
+  }
 }
