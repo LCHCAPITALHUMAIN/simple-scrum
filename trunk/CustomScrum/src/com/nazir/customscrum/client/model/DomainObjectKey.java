91f4f30f395f8de7fa0a4b913e6c8bb5d0d525b2
mail2nazir@gmail.com <mail2nazir@gmail.com@b9785878-aa22-11de-8882-bb5941c2e707>
2011-02-24 22:53:58 +0000

    Added holidays

---
+++ trunk/CustomScrum/src/com/nazir/customscrum/client/model/DomainObjectKey.java
@@ -0,0 +1,13 @@
+package com.nazir.customscrum.client.model;
+
+import java.io.Serializable;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: nazir
+ * Date: Feb 24, 2011
+ * Time: 10:46:14 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public interface DomainObjectKey extends Serializable{
+}
