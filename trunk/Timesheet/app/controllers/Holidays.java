0205e66946366488913e34eba5f8ab157100120d
mail2nazir@gmail.com <mail2nazir@gmail.com@b9785878-aa22-11de-8882-bb5941c2e707>
2012-03-21 08:49:27 +0000

    Added security

--- trunk/Timesheet/app/controllers/Holidays.java
+++ trunk/Timesheet/app/controllers/Holidays.java
@@ -1,5 +1,7 @@
 package controllers;
 
+import play.mvc.With;
+
 /**
  * Created by IntelliJ IDEA.
  * User: nazir
@@ -7,5 +9,6 @@ package controllers;
  * Time: 6:11 PM
  * To change this template use File | Settings | File Templates.
  */
+@With(Secure.class)
 public class Holidays extends CRUD{
 }
